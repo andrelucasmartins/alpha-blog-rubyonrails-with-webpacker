72c49ecd7b7791c995a122cf992b5b859bd253e6
André Ezequiel <andre.ezequiel@solvian.com>
2018-12-05 10:50:50 -0200

    Outher files the of project

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -7,20 +7,14 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-import 'stylesheets/application'
+
 
 $(function () {
     console.log('Hello World from Webpacker');
 });
-
+import 'bootstrap';
 import Rails from 'rails-ujs';
 import Turbolinks from 'turbolinks';
-import 'jquery/src/jquery'
-import 'jquery-ujs/src/rails'
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme'
-import 'bootstrap/dist/js/bootstrap';
-import 'style/application.scss'
 
 Rails.start();
 Turbolinks.start();
