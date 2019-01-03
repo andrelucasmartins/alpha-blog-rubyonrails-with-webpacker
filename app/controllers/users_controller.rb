7231668d4b34896b10b1fc7f2837e412eb687f55
André Ezequiel <andre.ezequiel@solvian.com>
2019-01-03 10:52:45 -0200

    Add sing in upon sing up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,9 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:success] = "Welcome to the alpha blog  #{@user.username}"
-            redirect_to articles_path
+            redirect_to user_path(@user)
         else
             render 'new'
         end
@@ -49,4 +50,5 @@ class UsersController < ApplicationController
             flash[:danger] = "You can only edit your own account"
             redirect_to root_path
         end
+    end
 end
