7204df283348c6bacb60c00307226ce74a015be0
André Ezequiel <andre.ezequiel@solvian.com>
2019-01-14 14:57:15 -0200

    Integration Test for Invalid Category

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -11,4 +11,15 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
         assert_template 'categories/index'
         assert_match "sports", response.body
     end
+
+    test "invalid category submission results in failure" do
+        get new_category_path
+        assert_template 'categories/new'
+        assert_no_difference 'Category.count', 1 do
+            post categories_path, params: {category: {name: " "}}
+        end
+        assert_template 'categories/new'
+        assert_select 'h2.card-title'
+        assert_select 'div.card-body'
+    end
 end
