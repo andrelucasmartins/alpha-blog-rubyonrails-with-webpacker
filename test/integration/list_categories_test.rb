2d5a9a9a7427becffb6054c7425291932dd822ef
André Ezequiel <andre.ezequiel@solvian.com>
2019-01-15 09:12:08 -0200

    Fix list categories test bug

--- test/integration/list_categories_test.rb
+++ test/integration/list_categories_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ListCategoriesTest < ActionDispatch::IntegrationTest
     def setup
-        @category = Category.create(name: "books")
+        @category = Category.create(name: "sports")
         @category2 = Category.create(name: "programming")
     end
 
@@ -10,6 +10,6 @@ class ListCategoriesTest < ActionDispatch::IntegrationTest
         get categories_path
         assert_template 'categories/index'
         assert_select "a[href=?]", category_path(@category), text: @category.name
-        assert_select "a[href=?]", category_path(@category), text: @category2.name
+        assert_select "a[href=?]", category_path(@category2), text: @category2.name
     end
 end
